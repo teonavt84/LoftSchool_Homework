287ea19acd1afdf5cb7e80756f10d94f81ec23ca
Denis <denis.volobuev1984@yandex.ru>
2023-05-10 14:22:33 +0300

    Fix Task5.1

--- 3 Week/Task5.1.py
+++ 3 Week/Task5.1.py
@@ -14,22 +14,26 @@
 # <function_name>(["Мумбаи", "Индия", 19980000, 23645000, 881,]) -> {"country":"Индия", "town": "Мумбаи","population":23645000, "square":881}
 # <function_name>(["Циндао", "Китай", 5381000,]) -> ValueError
 import LSpt5_t1_data as data
+
 data_country = data.data
 
+i = 0
 
-def country_dict(data_list: list) -> list:
-    result = []
+
+def country_dict(data_list: list) -> dict:
     for element in data_list:
+        if len(element) < 5:
+            raise ValueError
+        dict_county = {'Country': element[1], 'Town': element[0], 'Population': max(element[2:4]),
+                       'Square': element[4]}
+    return dict_county
+
+
+while i < len(data_country):
+    for i, el in enumerate(data_country):
         try:
-            if len(element) < 5:
-                raise ValueError
-            dict_county = {'Country': element[1], 'Town': element[0], 'Population': max(element[2:4]),
-                           'Square': element[4]}
-            result.append(dict_county)
+            print(f'{el} -> {country_dict([el])}')
         except ValueError:
-            result.append('ValueError')
-    return result
+            print(f'{el} -> ValueError')
+    i += 1
 
-
-for i, el in enumerate(data_country):
-    print(f'{el} -> {country_dict(data_country)[i]}')
